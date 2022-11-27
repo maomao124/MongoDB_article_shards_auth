cd57277dd0ff767d9ed259d0a53a002628ea37f6
mao <1296193245@qq.com>
2022-11-27 21:40:56 +0800

    MongoDB_article_shards_auth

---
+++ src/main/java/mao/mongodb_article_shards_auth/MongoDbArticleShardsAuthApplication.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_shards_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbArticleShardsAuthApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoDbArticleShardsAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/mongodb_article_shards_auth/MongoDbArticleShardsAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_shards_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbArticleShardsAuthApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
